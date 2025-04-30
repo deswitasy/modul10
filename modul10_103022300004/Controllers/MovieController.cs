a9f0da7b0fe6c9d6cc0807d6c1ac028cc5a1d4ef
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-04-30 07:52:51 +0700

    menambahkan API Movies

---
+++ modul10_103022300004/Controllers/MovieController.cs
@@ -0,0 +1,49 @@
+﻿using System.Net.NetworkInformation;
+using Microsoft.AspNetCore.Mvc;
+using modul10_103022300004.Models;
+using Microsoft.AspNetCore.Http;
+
+namespace modul10_103022300004.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MovieController : Controller
+    {
+        private static List<Movie> movieList = new List<Movie>
+        {
+            new Movie ("The Shawshank Redemption", "Frank Darabont", new List<string> { "Tim Robbins" , "Morgan Freeman", "Bob Gunton"}, "A banker convicted of uxoricide forms a friendship over a quarter century with a hardened convict, while maintaining his innocence and trying to remain hopeful through simple compassion."),
+            new Movie ("The GodFather", "Francis Ford Coppola",  new List<string> {"Marlon Brando", "Al Pacino", "James Caan" }, "The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son."),
+            new Movie ("The Dark Knight", "Christopher Nolan", new List<string>{"Christian Bale", "Heath Ledger", "Aaron Eckhart"}, "When a menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman, James Gordon and Harvey Dent must work together to put an end to the madness.")
+        };
+        [HttpGet]
+        public ActionResult<IEnumerable<Movie>> Get()
+        {
+            return Ok(movieList);
+        }
+        [HttpGet("{index}")]
+        public ActionResult<Movie> Get(int index)
+        {
+            if (index < 0 || index >= movieList.Count)
+            {
+                return NotFound();
+            }
+            return Ok(movieList[index]);
+        }
+        [HttpPost]
+        public void Post([FromBody] Movie movieBaru)
+        {
+            movieList.Add(movieBaru);
+
+        }
+        [HttpDelete]
+        public ActionResult Delete(int index)
+        {
+            if (index < 0 || index >= movieList.Count)
+            {
+                return NotFound();
+            }
+            movieList.RemoveAt(index);
+            return Ok();
+        }
+    }
+}
---
+++ modul10_103022300004/Models/Movie.cs
@@ -0,0 +1,18 @@
+﻿namespace modul10_103022300004.Models
+{
+    public class Movie
+    {
+        public string title { get; set; }
+        public string Director { get; set; }
+        public List<string> stars { get; set; }
+        public string Description { get; set; }
+
+        public Movie (string title, string director, List<string> stars, string description)
+        {
+            this.title = title;
+            Director = director;
+            this.stars = stars;
+            Description = description;
+        }
+    }
+}
